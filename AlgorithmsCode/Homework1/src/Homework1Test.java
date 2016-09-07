cd4f27057db99635d91db05ddbffbfe8a7362a71
PimmyTrousers <pim.trouerbach1@gmail.com>
2016-09-07 16:10:13 -0700

    Added all the stuffz

---
+++ AlgorithmsCode/Homework1/src/Homework1Test.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Pim on 9/5/16.
+ */
+public class Homework1Test {
+    @org.junit.Test
+    public void isCircularPermutation() throws Exception {
+        String string1 = "abcd";
+        String string2 = "abcd";
+        Boolean value = false;
+        value = Homework1.isCircularPermutation(string1, string2);
+        assertEquals(value, Boolean.TRUE);
+        string2 = "abde";
+        value = Homework1.isCircularPermutation(string1, string2);
+        assertEquals(value, Boolean.FALSE);
+        string2 = "cdab";
+        value = Homework1.isCircularPermutation(string1, string2);
+        assertEquals(value, Boolean.TRUE);
+        string2 = "abcde";
+        value = Homework1.isCircularPermutation(string1, string2);
+        assertEquals(value, Boolean.FALSE);
+        /*String string3 = null;
+        string2 = "";
+        value = Homework1.isCircularPermutation(string3, string2); */
+    }
+
+}
