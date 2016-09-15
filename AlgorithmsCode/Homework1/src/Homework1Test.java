c2511acaac8169fe8bc500b04c9655a366dc24eb
PimmyTrousers <pim.trouerbach1@gmail.com>
2016-09-14 19:26:42 -0700

    Added notes for class on 9/14/16

--- AlgorithmsCode/Homework1/src/Homework1Test.java
+++ AlgorithmsCode/Homework1/src/Homework1Test.java
@@ -8,7 +8,11 @@ public class Homework1Test {
     public void isCircularPermutation() throws Exception {
         String string1 = "abcd";
         String string2 = "abcd";
+        String string3 = "aab";
+        String string4 = "aba";
         Boolean value = false;
+        value = Homework1.isCircularPermutation(string3, string4);
+        assertEquals(value, TRUE);
         value = Homework1.isCircularPermutation(string1, string2);
         assertEquals(value, Boolean.TRUE);
         string2 = "abde";
