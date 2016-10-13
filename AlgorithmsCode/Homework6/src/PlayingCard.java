7f1bde1d2d4b9a154f72db1fbcc0cd2c2f0f9aa2
PimmyTrousers <pim.trouerbach1@gmail.com>
2016-10-12 19:29:33 -0700

    Added code for homework 6 and notes through 10/13

--- AlgorithmsCode/Homework6/src/PlayingCard.java
+++ AlgorithmsCode/Homework6/src/PlayingCard.java
@@ -1,53 +1,134 @@
-package Homework6;
-
 import java.util.Iterator;
-import java.util.Random;
+
+
+/***
+ * Made my Pim Trouerbach
+ *
+ */
+
+
+
 
 public class PlayingCard implements Comparable<PlayingCard> {
-	
-	public enum  Rank { Two, Three, Four, Five, Six,
+
+
+    public enum  Rank { Two, Three, Four, Five, Six,
 		Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
-	
+
 	public enum Suit { Clubs, Diamonds, Hearts, Spades }
 
 	private Rank _r;
 	private Suit _s;
-	
+
 	public PlayingCard(Rank r, Suit s) {
 		this._r=r;
 		this._s=s;
 	}
-	
+
 	public Rank rank() {
 
 		return this._r;
 	}
-	
+
 	public Suit suit() {
 
 		return this._s;
 	}
-	
+
+
+	/**
+	 * Rearranges an array of objects in uniformly random order
+	 * (under the assumption that {@code Math.random()} generates independent
+	 * and uniformly distributed numbers between 0 and 1).
+	 * @param a the array to be shuffled
+	 * @source http://algs4.cs.princeton.edu/11model/Knuth.java.html
+	 */
+	public static void knuthShuffle(Object[] a) {
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			int r = i + (int) (Math.random() * (n - i));
+			Object swap = a[r];
+			a[r] = a[i];
+			a[i] = swap;
+		}
+	}
+
 	/***
-	 * 
-	 * @return an Iterator over a set of 52 PlayingCard objects in 
+	 *
+	 * @return an Iterator over a set of 52 PlayingCard objects in
 	 * their natural order
 	 */
-	public static Iterator<PlayingCard> OrderedDeck() {
-		// TODO complete this for assignment
-		return null;
+	public static Iterator<PlayingCard> OrderedDeck() { //
+        final PlayingCard[] deck = new PlayingCard[52];
+        int counter = 0;
+        int currentPos = 0;
+        for(int i = 0; i < 4; i++){
+            Suit s = Suit.values()[i];
+            for(int j=0; j< 13; j++){
+                Rank r = Rank.values()[j];
+                PlayingCard card = new PlayingCard(r, s);
+                deck[counter] = card;
+                counter++;
+            }
+        }
+        Iterator<PlayingCard> itr = new Iterator<PlayingCard>() {
+            int currentPos = 0;
+            @Override
+            public boolean hasNext() {
+                return (currentPos < deck.length);
+            }
+
+            @Override
+            public PlayingCard next() {
+                return deck[currentPos++];
+            }
+
+			@Override
+			public void remove() {
+
+			}
+		};
+        return itr;
 	}
-	
+
+
 	/***
-	 * 
-	 * @return an Iterator over a set of 52 PlayingCard objects 
+	 *
+	 * @return an Iterator over a set of 52 PlayingCard objects
 	 *  in a randomized order
 	 */
 	public static Iterator<PlayingCard> ShuffledDeck() {
-		//TODO complete this for assignment
-		return null;
+        final PlayingCard[] deck = new PlayingCard[52];
+        int counter = 0;
+        int currentPos = 0;
+        for(int i = 0; i < 4; i++){
+            Suit s = Suit.values()[i];
+            for(int j=0; j< 13; j++){
+                Rank r = Rank.values()[j];
+                PlayingCard card = new PlayingCard(r, s);
+                deck[counter] = card;
+                counter++;
+            }
+        }
+        knuthShuffle(deck);
+        Iterator<PlayingCard> itr = new Iterator<PlayingCard>() {
+			int currentPos = 0;
+            @Override
+            public boolean hasNext(){ return (currentPos < deck.length); }
+
+
+            @Override
+            public PlayingCard next() { return deck[currentPos++]; }
+
+			@Override
+			public void remove() {
+
+			}
+
+		};
+    return itr;
 	}
-	
+
 	/***
 	 * asssumptions for natural ordering:
 	 *   Rank:  Two < Three < ..Ten < Jack < Queen < King < Ace
@@ -55,8 +136,19 @@ public class PlayingCard implements Comparable<PlayingCard> {
 	 */
 	@Override
 	public int compareTo(PlayingCard o) {
-		// TODO complete this for assignment
-		return 0;
+		if (this.rank().compareTo(o.rank()) < 0) {
+			return -1;
+		} else if (this.rank().compareTo(o.rank()) > 0) {
+			return 1;
+		} else {
+			if (this.suit().compareTo(o.suit()) < 0) {
+				return -1;
+			} else if (this.suit().compareTo(o.suit()) > 0) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
 	}
 
 }
--- AlgorithmsCode/Homework6/src/PlayingCardTest.java
+++ AlgorithmsCode/Homework6/src/PlayingCardTest.java
@@ -1,21 +1,65 @@
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by Pim on 10/5/16.
+ * Created by Pim Trouerbach on 10/5/16.
  */
 public class PlayingCardTest {
     @org.junit.Test
     public void orderedDeck() throws Exception {
-
+        Iterator<PlayingCard> cardIterator = PlayingCard.OrderedDeck();
+        for(int i = 0; i < 52; i++){
+            PlayingCard card = cardIterator.next();
+            System.out.println(card.rank()+ " of " + card.suit());
+        }
     }
 
     @org.junit.Test
     public void shuffledDeck() throws Exception {
-
+        Iterator<PlayingCard> cardIterator = PlayingCard.ShuffledDeck();
+        for(int i = 0; i < 52; i++){
+            PlayingCard card = cardIterator.next();
+            System.out.println(card.rank()+ " of " + card.suit());
+        }
     }
 
     @org.junit.Test
     public void compareTo() throws Exception {
+        PlayingCard card = new PlayingCard(PlayingCard.Rank.Two, PlayingCard.Suit.Diamonds);
+        PlayingCard card1 = new PlayingCard(PlayingCard.Rank.Eight, PlayingCard.Suit.Diamonds);
+        PlayingCard card2 = new PlayingCard(PlayingCard.Rank.Eight, PlayingCard.Suit.Diamonds);
+        PlayingCard card3 = new PlayingCard(PlayingCard.Rank.Eight, PlayingCard.Suit.Diamonds);
+        PlayingCard card4 = new PlayingCard(PlayingCard.Rank.Eight, PlayingCard.Suit.Hearts);
+        PlayingCard card5 = new PlayingCard(PlayingCard.Rank.Eight, PlayingCard.Suit.Spades);
+        PlayingCard card6 = new PlayingCard(PlayingCard.Rank.Ace, PlayingCard.Suit.Clubs);
+        PlayingCard card7 = new PlayingCard(PlayingCard.Rank.Two, PlayingCard.Suit.Hearts);
+        PlayingCard card8 = new PlayingCard(PlayingCard.Rank.Eight, PlayingCard.Suit.Hearts);
+
+
+        int result = card.compareTo(card1);
+        System.out.println(result);
+        assertEquals(result, -1);
+        result = card2.compareTo(card3);
+        System.out.println(result);
+        assertEquals(result, 0);
+        result = card4.compareTo(card3);
+        System.out.println(result);
+        assertEquals(result, 1);
+        result = card5.compareTo(card4);
+        System.out.println(result);
+        assertEquals(result, 1);
+        result = card7.compareTo(card8);
+        System.out.println(result);
+        assertEquals(result, -1);
+        result = card8.compareTo(card7);
+        System.out.println(result);
+        assertEquals(result, 1);
+        result = card6.compareTo(card);
+        System.out.println(result);
+        assertEquals(result, 1);
+
+
 
     }
 
